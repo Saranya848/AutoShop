a2768db8fdfd1cb7b7e139f731745576ee6bb0b5
SaranyaSiripurapu <85158690+SaranyaSiripurapu@users.noreply.github.com>
2022-03-10 01:03:21 +0530

    Update Ford.java

--- src/com/ownautoshop/Ford.java
+++ src/com/ownautoshop/Ford.java
@@ -4,13 +4,25 @@ public class Ford extends Car {
 	int year;
 	static int manufacturerDiscount;
 
+	/**
+	 * constructor class
+	 * 
+	 * @param speed                - super property accessed of spped
+	 * @param regularPrice         - super property accessed of regularprice
+	 * @param color                - super property accessed of color
+	 * @param year                 - super property accessed of year
+	 * @param manufacturerDiscount - super property accessed of manufacturerDiscount
+	 */
 	public Ford(int speed, double regularPrice, String color, int year, int manufacturerDiscount) {
 		super(speed, regularPrice, color);
 		this.year = year;
 		Ford.manufacturerDiscount = manufacturerDiscount;
 	}
 
-	public static double getSalePrice(double regularPrice) {
-		return (Car.getSalePrice(regularPrice) - manufacturerDiscount);
+	/**
+	 * method for getting salesprice of Ford
+	 */
+	public double getSalePrice() {
+		return (super.getSalePrice() - manufacturerDiscount);
 	}
 }
